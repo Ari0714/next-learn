a81dccf4d2101c50a7cf6effb6b115f0cf74def9
ari.chen <ari.chen.cn@gmail.com>
2025-07-01 20:04:24 +0800

    test: github-action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    output: 'export', // 关键设置
+    basePath: isProd ? '/next-learn' : '',
+    assetPrefix: isProd ? '/next-learn/' : '',
+};
 
 export default nextConfig;
+
+// module.exports = {
+//     output: 'export', // 启用静态导出模式
+// };
+
